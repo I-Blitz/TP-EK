381398875f069c0124eac478c86de246756ca025
I-Blitz <imad.bda@gmail.com>
2022-09-05 14:56:23 +0200

    update operateur << de polynôme

--- include/polynome.hpp
+++ include/polynome.hpp
@@ -14,14 +14,27 @@ class Polynome{
     public:
         Polynome(int coeff = 0, unsigned degre = 0); // constructeur de base
         Polynome(int coeff[], unsigned degre[], int n); 
+        Polynome(std::vector<int>& vec_coeff, std::vector<unsigned int>& vec_degre);
         Polynome(const MonoVec &data0); 
 
         Monome afficher_monome(int i) const;
         void simplifie();
         int size() const;
+
+        double operator()(double x) const; // surcharge interne
+        bool operator==(Polynome const& Q) const; // surcharge interne
+        bool operator<(Polynome const& Q) const; // surcharge interne
+        friend const Polynome operator+(Polynome const& P, Polynome const& Q); // surcharge externe 
+
+        Monome get_monome(long long unsigned int ith_element);
+        
     private:
         MonoVec data;
 
 };
+
+
+const Polynome operator-(Polynome const& P, Polynome const& Q); // surcharge externe
+std::ostream& operator<<(std::ostream& sortie, Polynome const Q); // surcharge externe 
 
 #endif
--- src/test_polynome.cpp
+++ src/test_polynome.cpp
@@ -2,8 +2,8 @@
 #include "polynome.hpp"
 
 int main(){
-    std::cout << "Hello World" << std::endl;
 
+    // test constructeur
     int coeff[] = { 2, -1, 4, 0, 2, 6 }; 
     unsigned degre [] = { 0, 3, 2, 5, 6, 2 };
     Polynome P(coeff , degre , 6);
@@ -19,6 +19,17 @@ int main(){
     for(int i= 0; i < P.size() ; ++i){
         std::cout << P.afficher_monome(i) << " " ;
     }
+    std::cout << std::endl;
 
+    // test surcharge de l'opérateur + 
+    Polynome Q = P+P;
+    std::cout << "Polynome Q = P+P : " ;
+    for(int i= 0; i < Q.size() ; ++i){
+        std::cout << Q.afficher_monome(i) << " " ;
+    }
+    std::cout << std::endl;
+
+    // test surcharge de l'opérateur //
+    std::cout << "P = " << P << std::endl;
     return(0);
 }
